f722f733842d751b32af491042fba53fcb9570ab
zacazaca <guilherme.souza@provafacilnaweb.com.br>
2023-10-25 18:20:08 -0300

    melhorendo a organização

--- curso_udemy/fundamentos/primeiro_projeto_pyhon/fundamentos_projetos/area_circunferencia_v4y.py
+++
@@ -1,7 +0,0 @@
-import math
-print(dir(math))
-#print(f"valor de PI: {math.pi}")
-
-pi = math.pi
-raio = 15.3
-
--- curso_udemy/fundamentos/primeiro_projeto_pyhon/fundamentos_projetos/area_circunferencia_v5.py
+++
@@ -1,7 +0,0 @@
-# Aula 64 Obtendo ados do usuário
-import math
-# print(dir(math))
-pi = math.pi
-# -> Foi realizado um conversão explicita
-raio = float(input("Informe o raio: "))
-print(f"A área do circulo: {pi * raio ** 2}")
